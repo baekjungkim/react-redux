9549027f106095e6ccb63e4f6d4cf73c353fb84e
baekjung <zvgandam@gmail.com>
2020-08-06 17:58:59 +0900

    Redux Toolkit CreateAction

--- src/store.js
+++ src/store.js
@@ -1,29 +1,46 @@
 import { createStore } from "redux";
+import { createAction } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const REMOVE = "REMOVE";
+// const ADD = "ADD";
+// const REMOVE = "REMOVE";
 
-const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
+// const addToDo = (text) => {
+//   return {
+//     type: ADD,
+//     text,
+//   };
+// };
 
-const removeToDo = (id) => {
-  return {
-    type: REMOVE,
-    id,
-  };
-};
+// const removeToDo = (id) => {
+//   return {
+//     type: REMOVE,
+//     id,
+//   };
+// };
+
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case ADD:
+//       return [{ text: action.text, id: Date.now() }, ...state];
+//     case REMOVE:
+//       return state.filter((toDo) => {
+//         return toDo.id !== action.id;
+//       });
+//     default:
+//       return state;
+//   }
+// };
+
+const addToDo = createAction("ADD");
+const removeToDo = createAction("REMOVE");
 
 const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case REMOVE:
+    case addToDo.type:
+      return [{ text: action.payload, id: Date.now() }, ...state];
+    case removeToDo.type:
       return state.filter((toDo) => {
-        return toDo.id !== action.id;
+        return toDo.id !== action.payload;
       });
     default:
       return state;

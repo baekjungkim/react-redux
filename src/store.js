ac2f2e8cbdf1f78617787474d9e8c8202f346ad0
baekjung <zvgandam@gmail.com>
2020-08-06 18:37:05 +0900

    createReducer

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 // const ADD = "ADD";
 // const REMOVE = "REMOVE";
@@ -31,21 +31,31 @@ import { createAction } from "@reduxjs/toolkit";
 //   }
 // };
 
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case addToDo.type:
+//       return [{ text: action.payload, id: Date.now() }, ...state];
+//     case removeToDo.type:
+//       return state.filter((toDo) => {
+//         return toDo.id !== action.payload;
+//       });
+//     default:
+//       return state;
+//   }
+// };
+
 const addToDo = createAction("ADD");
 const removeToDo = createAction("REMOVE");
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case addToDo.type:
-      return [{ text: action.payload, id: Date.now() }, ...state];
-    case removeToDo.type:
-      return state.filter((toDo) => {
-        return toDo.id !== action.payload;
-      });
-    default:
-      return state;
-  }
-};
+// Redux Toolkit 으로 Reducer를 짤때는 state mutate 가능
+// but, state mutate 시에는 no return, 새 state 생성은 return
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() }); // state mutate : no return
+  },
+  [removeToDo]: (state, action) =>
+    state.filter((toDo) => toDo.id !== action.payload), // new state : return
+});
 
 const store = createStore(reducer);
 
